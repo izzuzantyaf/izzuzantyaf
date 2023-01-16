7d26d21687c76274ccdb9c73d71918e8a47ccaa5
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2023-01-16 23:50:10 +0700

    feat: update portfolio

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,6 +1,7 @@
 import fisdasWebThumbnail from "../public/images/projects/labfisdas-telu.com_.png";
 import fisdasCMSThumbnail from "../public/images/projects/fisdascms.vercel.app_.png";
 import presisiThumbnail from "../public/images/projects/presisi.vercel.app_.png";
+import newsHomePageThumbnail from "../public/images/projects/izzuzantyaf.github.io_news-homepage_.png";
 
 export const projects = [
   {
@@ -20,5 +21,11 @@ export const projects = [
     description: "How to learn recommendation web app",
     link: "https://presisi.vercel.app",
     thumbnailSrc: presisiThumbnail,
+  },
+  {
+    name: "News Homepage",
+    description: "News homepage challenge solution from Frontendmentor",
+    link: "https://izzuzantyaf.github.io/news-homepage",
+    thumbnailSrc: newsHomePageThumbnail,
   },
 ];

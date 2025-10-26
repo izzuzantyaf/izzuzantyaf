8edcaa7bae281dfe01847605d702ba0efdbdf0ef
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2025-10-26 15:47:48 +0700

    feat: refactor social media links into a reusable array for better maintainability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,25 @@
 import { MdAlternateEmail } from "react-icons/md";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
+import { IconBaseProps } from "react-icons";
+
+const SOCIAL_MEDIA = [
+  {
+    name: "Email",
+    icon: (props: IconBaseProps) => <MdAlternateEmail {...props} />,
+    href: "mailto:izzuzantyaf+work@gmail.com",
+  },
+  {
+    name: "LinkedIn",
+    icon: (props: IconBaseProps) => <FaLinkedin {...props} />,
+    href: "https://www.linkedin.com/in/izzuzantyaf/",
+  },
+  {
+    name: "GitHub",
+    icon: (props: IconBaseProps) => <FaGithub {...props} />,
+    href: "https://github.com/izzuzantyaf",
+  }
+]
 
 export default function Home() {
   return (
@@ -20,15 +39,11 @@ export default function Home() {
 
         <p className="text-lg sm:text-xl text-gray-600 mb-4">Let&apos;s get connected</p>
         <div className="flex gap-4">
-          <a href="mailto:izzuzantyaf+work@gmail.com" className="p-2 rounded-xl bg-gray-200 transition-all duration-300 hover:scale-110">
-            <MdAlternateEmail className="text-2xl sm:text-3xl" />
-          </a>
-          <a href="https://www.linkedin.com/in/izzuzantyaf/" target="_blank" rel="noopener noreferrer" className="p-2 rounded-xl bg-gray-200 transition-all duration-300 hover:scale-110">
-            <FaLinkedin className="text-2xl sm:text-3xl" />
-          </a>
-          <a href="https://github.com/izzuzantyaf" target="_blank" rel="noopener noreferrer" className="p-2 rounded-xl bg-gray-200 transition-all duration-300 hover:scale-110">
-            <FaGithub className="text-2xl sm:text-3xl" />
-          </a>
+          {SOCIAL_MEDIA.map((item) => (
+            <a key={item.name} href={item.href} target="_blank" rel="noopener noreferrer" className="p-2 rounded-xl bg-gray-200 transition-all duration-300 hover:scale-110">
+              <item.icon className="text-2xl sm:text-3xl" />
+            </a>
+          ))}
         </div>
       </section>
     </>

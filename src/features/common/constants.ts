2a75d88f60ca88d2fd50d636875bf8f41ff7e691
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2026-06-11 21:31:43 +0700

    style: adjust Biome rules

--- src/features/common/constants.ts
+++ src/features/common/constants.ts
@@ -1,31 +1,31 @@
-import fisdasWebThumbnail from "../public/images/projects/labfisdas-telu.com_.png";
-import fisdasCMSThumbnail from "../public/images/projects/fisdascms.vercel.app_.png";
-import presisiThumbnail from "../public/images/projects/presisi.vercel.app_.png";
-import newsHomePageThumbnail from "../public/images/projects/izzuzantyaf.github.io_news-homepage_.png";
+import fisdasCMSThumbnail from '../public/images/projects/fisdascms.vercel.app_.png'
+import newsHomePageThumbnail from '../public/images/projects/izzuzantyaf.github.io_news-homepage_.png'
+import fisdasWebThumbnail from '../public/images/projects/labfisdas-telu.com_.png'
+import presisiThumbnail from '../public/images/projects/presisi.vercel.app_.png'
 
 export const projects = [
   {
-    name: "Fisdas Web",
-    description: "Website of Physics Lab Telkom University",
-    link: "https://labfisdas-telu.com",
+    name: 'Fisdas Web',
+    description: 'Website of Physics Lab Telkom University',
+    link: 'https://labfisdas-telu.com',
     thumbnailSrc: fisdasWebThumbnail,
   },
   {
-    name: "Fisdas CMS",
-    description: "CMS Website of Physics Lab Telkom University",
-    link: "https://fisdascms-demo.vercel.app",
+    name: 'Fisdas CMS',
+    description: 'CMS Website of Physics Lab Telkom University',
+    link: 'https://fisdascms-demo.vercel.app',
     thumbnailSrc: fisdasCMSThumbnail,
   },
   {
-    name: "Presisi",
-    description: "How to learn recommendation web app",
-    link: "https://presisi.vercel.app",
+    name: 'Presisi',
+    description: 'How to learn recommendation web app',
+    link: 'https://presisi.vercel.app',
     thumbnailSrc: presisiThumbnail,
   },
   {
-    name: "News Homepage",
-    description: "News homepage challenge solution from Frontendmentor",
-    link: "https://izzuzantyaf.github.io/news-homepage",
+    name: 'News Homepage',
+    description: 'News homepage challenge solution from Frontendmentor',
+    link: 'https://izzuzantyaf.github.io/news-homepage',
     thumbnailSrc: newsHomePageThumbnail,
   },
-];
+]

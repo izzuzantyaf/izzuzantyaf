f2728e535829d37becf4811d2c413b15ec165acf
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2026-06-11 21:31:43 +0700

    style: adjust Biome rules

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,16 +1,13 @@
+import { TanStackDevtools } from '@tanstack/react-devtools'
+import type { QueryClient } from '@tanstack/react-query'
 import {
+  createRootRouteWithContext,
   HeadContent,
   Scripts,
-  createRootRouteWithContext,
 } from '@tanstack/react-router'
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
-import { TanStackDevtools } from '@tanstack/react-devtools'
-
 import TanStackQueryDevtools from '../integrations/tanstack-query/devtools'
-
 import appCss from '../styles.css?url'
-
-import type { QueryClient } from '@tanstack/react-query'
 
 interface MyRouterContext {
   queryClient: QueryClient
@@ -27,7 +24,7 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
         content: 'width=device-width, initial-scale=1',
       },
       {
-        title: 'TanStack Start Starter',
+        title: 'Izzu Z. Fawwas | AI-Native Software Engineer',
       },
     ],
     links: [
